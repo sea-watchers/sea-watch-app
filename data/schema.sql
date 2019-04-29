9e13dd559fa3a742448a98003b9bc4526880ee7b
mspake <morgana.spake@gmail.com>
2019-04-29 14:29:25 -0700

    schema fixed

--- data/schema.sql
+++ data/schema.sql
@@ -1,17 +1,18 @@
 DROP TABLE IF EXISTS saved_searches;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE saved_searches (
-  id SERIAL PRIMARY KEY,
-  location VARCHAR(255),
-  lat NUMERIC,
-  lng NUMERIC,
-  user INTEGER,
-  ADD FOREIGN KEY (user) REFERENCES users(id)
-);
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   user_name VARCHAR(255)
 );
+
+CREATE TABLE saved_searches (
+  id SERIAL PRIMARY KEY,
+  location VARCHAR(255),
+  lat NUMERIC(10, 7),
+  lng NUMERIC(10, 7),
+  person INTEGER,
+  FOREIGN KEY (person) REFERENCES users(id)
+);
+
 
