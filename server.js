5815408190e888bf709ab50d4baa2e933f8503a6
mspake <morgana.spake@gmail.com>
2019-04-30 12:43:51 -0700

    hitting all apis

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ function searchLocation(query, response) {
   superagent.get(URL).then(result => {
     const lat = result.body.results[0].geometry.location.lat;
     const lng = result.body.results[0].geometry.location.lng;
+    console.log('location = ', lat, lng);
 
     searchAquaplot(lat, lng);
     searchStormGlass(lat, lng);
@@ -99,26 +100,44 @@ function searchLocation(query, response) {
 }
 
 function searchAquaplot(lat, lng) {
-  console.log(lat, lng);
-  const URL = `https://api.aquaplot.com/v1/validate/${lng}/${lat}`
+  const URL = `https://api.aquaplot.com/v1/validate/${lng}/${lat}`;
   superagent.get(URL).auth(process.env.AQUAPLOT_API_USERNAME, process.env.AQUAPLOT_API_KEY, { type: 'auto' }).then(result => {
+    console.log('aquaplot');
     console.log(result.body);
   });
-  //
 }
 
 function searchStormGlass(lat, lng) {
-  //
+  const URL = `https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}`;
+  superagent.get(URL).set('Authorization', process.env.STORMGLASS_API_KEY).then(result => {
+    console.log('storm glass');
+    console.log(result.body.meta.start, result.body.meta.end)
+  })
 }
 
 function searchWorldWeather(lat, lng) {
-  //
+  const tide = 'yes'
+  const includelocation = 'yes'
+  const URL = `https://api.worldweatheronline.com/premium/v1/marine.ashx?key=${process.env.WWO_API_KEY}&q=${lat},${lng}&format=json&includelocation=${includelocation}&tide=${tide}`;
+  superagent.get(URL).then(result => {
+    console.log('world weather');
+    console.log(result.body.data.request);
+  });
 }
 
 function searchSolunar(lat, lng) {
-  //
+  const date = 20180207
+  const URL = `https://api.solunar.org/solunar/${lat},${lng},${date},-4`
+  superagent.get(URL).then(result => {
+    console.log('solunar');
+    console.log(result.body.moonPhase);
+  })
 }
 
 function searchSunriseSunset(lat, lng) {
-  //
+  const URL = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
+  superagent.get(URL).then(result => {
+    console.log('sunrise sunset');
+    console.log(result.body);
+  })
 }
