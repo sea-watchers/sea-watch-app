ba818b279f160cc69f42556b3aee42824356d79e
mspake <morgana.spake@gmail.com>
2019-04-30 14:07:02 -0700

    switched to asynch function and promises for api calls

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.set('view-engine', 'ejs');
 
 
 app.use(methodOverride((request, response) => {
-  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
-    let method = request.body._method;
-    delete request.body._method;
-    return method;
-  }
+    if (request.body && typeof request.body === 'object' && '_method' in request.body) {
+        let method = request.body._method;
+        delete request.body._method;
+        return method;
+    }
 }))
 
 //==================================
@@ -61,82 +61,92 @@ const SQL = {};
 //==================================
 
 function handleError(error, response) {
-  response.render('pages/error.ejs', { status: 500, message: `I'm sorry, something has gone wrong.` });
-  console.log(error);
+    response.render('pages/error.ejs', { status: 500, message: `I'm sorry, something has gone wrong.` });
+    console.log(error);
 }
 
 function renderLandingPage(request, response) {
-  response.render('pages/index.ejs')
+    response.render('pages/index.ejs')
 }
 
 function renderAboutPage(request, response) {
-  response.render('pages/about.ejs')
+    response.render('pages/about.ejs')
 }
 
 function renderResultsPage(request, response) {
-  const query = request.query.search;
-  searchLocation(query, response)
+    const query = request.query.search;
+    searchLocation(query, response)
 }
 
 function renderSavedSearches(request, response) {
-  response.render('pages/searches/saved_searches.ejs')
+    response.render('pages/searches/saved_searches.ejs')
 }
 
-function searchLocation(query, response) {
-  // query google API for location
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
-  superagent.get(URL).then(result => {
+async function searchLocation(query, response) {
+    // query google API for location
+    const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+    const result = await superagent.get(URL);
     const lat = result.body.results[0].geometry.location.lat;
     const lng = result.body.results[0].geometry.location.lng;
     console.log('location = ', lat, lng);
 
-    searchAquaplot(lat, lng);
-    searchStormGlass(lat, lng);
-    searchWorldWeather(lat, lng);
-    searchSolunar(lat, lng);
-    searchSunriseSunset(lat, lng);
-  }).catch(error => handleError(error, response));
+    const aqua = await searchAquaplot(lat, lng);
+    // const storm = await searchStormGlass(lat, lng);
+    const wwo = await searchWorldWeather(lat, lng);
+    const solunar = await searchSolunar(lat, lng);
+    const sunset = await searchSunriseSunset(lat, lng);
+    console.log(aqua, storm, wwo, solunar, sunset);
 }
 
 function searchAquaplot(lat, lng) {
-  const URL = `https://api.aquaplot.com/v1/validate/${lng}/${lat}`;
-  superagent.get(URL).auth(process.env.AQUAPLOT_API_USERNAME, process.env.AQUAPLOT_API_KEY, { type: 'auto' }).then(result => {
-    console.log('aquaplot');
-    console.log(result.body);
-  });
+    return new Promise(resolve => {
+        const URL = `https://api.aquaplot.com/v1/validate/${lng}/${lat}`;
+        superagent.get(URL).auth(process.env.AQUAPLOT_API_USERNAME, process.env.AQUAPLOT_API_KEY, { type: 'auto' }).then(result => {
+            resolve('aquaplot');
+            // console.log(result.body);
+        });
+    });
 }
 
 function searchStormGlass(lat, lng) {
-  const URL = `https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}`;
-  superagent.get(URL).set('Authorization', process.env.STORMGLASS_API_KEY).then(result => {
-    console.log('storm glass');
-    console.log(result.body.meta.start, result.body.meta.end)
-  })
+    return new Promise(resolve => {
+        const URL = `https://api.stormglass.io/v1/weather/point?lat=${lat}&lng=${lng}`;
+        superagent.get(URL).set('Authorization', process.env.STORMGLASS_API_KEY).then(result => {
+            resolve('storm glass');
+            // console.log(result.body.meta.start, result.body.meta.end)
+        });
+    });
 }
 
 function searchWorldWeather(lat, lng) {
-  const tide = 'yes'
-  const includelocation = 'yes'
-  const URL = `https://api.worldweatheronline.com/premium/v1/marine.ashx?key=${process.env.WWO_API_KEY}&q=${lat},${lng}&format=json&includelocation=${includelocation}&tide=${tide}`;
-  superagent.get(URL).then(result => {
-    console.log('world weather');
-    console.log(result.body.data.request);
-  });
+    return new Promise(resolve => {
+        const tide = 'yes'
+        const includelocation = 'yes'
+        const URL = `https://api.worldweatheronline.com/premium/v1/marine.ashx?key=${process.env.WWO_API_KEY}&q=${lat},${lng}&format=json&includelocation=${includelocation}&tide=${tide}`;
+        superagent.get(URL).then(result => {
+            resolve('world weather');
+            // console.log(result.body.data.request);
+        });
+    });
 }
 
 function searchSolunar(lat, lng) {
-  const date = 20180207
-  const URL = `https://api.solunar.org/solunar/${lat},${lng},${date},-4`
-  superagent.get(URL).then(result => {
-    console.log('solunar');
-    console.log(result.body.moonPhase);
-  })
+    return new Promise(resolve => {
+        const date = 20180207
+        const URL = `https://api.solunar.org/solunar/${lat},${lng},${date},-4`
+        superagent.get(URL).then(result => {
+            resolve('solunar');
+            // console.log(result.body.moonPhase);
+        });
+    });
 }
 
 function searchSunriseSunset(lat, lng) {
-  const URL = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
-  superagent.get(URL).then(result => {
-    console.log('sunrise sunset');
-    console.log(result.body);
-  })
+    return new Promise(resolve => {
+        const URL = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`
+        superagent.get(URL).then(result => {
+            resolve('sunrise sunset');
+            // console.log(result.body);
+        });
+    });
 }
