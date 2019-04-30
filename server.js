8891131bcd69aea25e9ee3517a9715f67609dd70
joseph <joseph.hangarter@yahoo.com>
2019-04-30 11:49:59 -0700

    scaffold results and about page; location from API rendered to search page

--- server.js
+++ server.js
@@ -76,7 +76,6 @@ function renderAboutPage(request, response){
 function renderResultsPage(request, response){
   const query = request.query.search;
   searchLocation(query, response)
-  response.render('pages/searches/results.ejs')
 }
 
 function renderSavedSearches(request, response){
@@ -85,8 +84,8 @@ function renderSavedSearches(request, response){
 
 function searchLocation(query, response){
   // query google API for location
-  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
   superagent.get(URL).then(result => {
-    console.log(result.body);
+    response.render('pages/searches/results.ejs', {data: result.body.results});
   }).catch(error => handleError(error, response));
 }
